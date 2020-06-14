807cc883e53ce73d249bfa083a0ce1abb15cdb92
romeoaxpuac123 <bayronaxpuac95@gmail.com>
2020-06-14 12:04:23 -0600

    Procesos Hijos

--- cpu_201314474_201314160/cpu_201314474_201314160.c
+++ cpu_201314474_201314160/cpu_201314474_201314160.c
@@ -14,43 +14,23 @@ MODULE_DESCRIPTION("SOFTWARE PARA VER EL ESTADO DEL CPU");
 MODULE_AUTHOR("Romeo Axpuac -  201314474  y Soy.LmmL 201314160");
 
 
-char buffer[256];
-char * get_task_state(long state)
-{
-    switch (state) {
-        case TASK_RUNNING:
-            return "TAREA_EJECUTANDO";
-        case TASK_INTERRUPTIBLE:
-            return "TAREA_INTERRUPTIBLE";
-        case TASK_UNINTERRUPTIBLE:
-            return "TAREA_ININTERRUPTIBLE";
-        case __TASK_STOPPED:
-            return "__TAREA_DETENIDA";
-        case __TASK_TRACED:
-            return "__TAREA_TRAZADA";
-        default:
-        {
-            sprintf(buffer, "ESTADO DESCONOCIDO:%ld\n", state);
-            return buffer;
-        }
-    }
-}
-
 
 
 static int escribir_archivo(struct seq_file * archivo, void *v) {	
    	
 	seq_printf(archivo, "  Carnet: 201314474_201314160                      \n");
     	seq_printf(archivo, "  Bayron Romeo Axpuac Yoc y Luis Manuel Morales Lopez\n");
-    	seq_printf(archivo, "  DOCH :D                 			   \n");
-	seq_printf(archivo, "  ID	PROCESOS			STATUS \n");
- 	struct task_struct* task_list;
-        size_t process_counter = 0;
-        for_each_process(task_list) {
-                seq_printf(archivo, "  [%d]	%s			%s\n",task_list->pid,task_list->comm,get_task_state(task_list->state));
-                ++process_counter;
-        }
-        printk(KERN_INFO "== Number of process: %zu\n", process_counter);
+    	seq_printf(archivo, "  DOCH S.A. :D                 			   \n");
+ 	struct task_struct *task;
+	struct task_struct *task_child;
+	struct list_head *list;
+	for_each_process( task ){
+		seq_printf(archivo,"\nID->:	%d PROCESO->:	%s ESTADO->:	%ld",task->pid, task->comm, task->state);
+		list_for_each(list, &task->children){
+			task_child = list_entry( list, struct task_struct, sibling );
+			seq_printf(archivo,"\n		PADRE->:	%s[%d] ID->:	%d PROCESO:->	%s ESTADO->:	%ld",task->comm, task->pid,task_child->pid, task_child->comm, task_child->state);
+		}
+	}
        
 	return 0;
 }
